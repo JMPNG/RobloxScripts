f275815a7d3658995eb535b231e479101e084308
James <42681518+jmesfo0@users.noreply.github.com>
2022-11-27 22:36:06 -0800

    Update psx-jmes.lua

--- psx-jmes.lua
+++ psx-jmes.lua
@@ -2,5 +2,5 @@ if game.PlaceId == 6284583030 then
   loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/jmesfo0/RobloxScripts/main/psx-jmes-normal.lua"))()
 end
 if game.PlaceId == 10321372166 then
-  loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/jmesfo0/RobloxScripts/main/psx-jmes-normal.lua"))()
+  loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/jmesfo0/RobloxScripts/main/psx-jmes-hardcore.lua"))()
 end
